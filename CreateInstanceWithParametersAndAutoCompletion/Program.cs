c0137831097980591df5e052ef3c71f51f0d33c8
lipchev <lipchev@gmail.com>
2020-05-18 16:53:29 +0300

    Example of  CreateInstanceWithParametersAndAutoCompletion<MyWorkPage>(myObject);

--- CreateInstanceWithParametersAndAutoCompletion/Program.cs
+++ CreateInstanceWithParametersAndAutoCompletion/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Catel;
+using Catel.IoC;
+using Catel.Logging;
 
 namespace CreateInstanceWithParametersAndAutoCompletion
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            LogManager.AddDebugListener();
+
+            var myConfig = new MyConfig();
+
+            var dr = DependencyResolverManager.Default.DefaultDependencyResolver;
+
+            var serviceLocator = dr.Resolve<IServiceLocator>();
+
+            serviceLocator.RegisterInstance(myConfig);
+
+            var typeFactory = dr.Resolve<ITypeFactory>();
+            try
+            {
+                // this works
+                Argument.IsNotNull("MyBasicPage", typeFactory.CreateInstanceWithParametersAndAutoCompletion<MyBasicPage>());
+
+                var myObject = new MyWorkObject();
+
+                // this does not
+                Argument.IsNotNull("MyWorkPage", typeFactory.CreateInstanceWithParametersAndAutoCompletion<MyWorkPage>(myObject));
+                // this would work
+                Argument.IsNotNull("MyWorkPage", typeFactory.CreateInstanceWithParametersAndAutoCompletion<MyWorkPage>(myConfig, myObject));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+
+        public class MyConfig
+        {
+        }
+
+        public class MyWorkObject
+        {
+        }
+
+        public class MyBasicPage
+        {
+            public MyBasicPage(MyConfig config)
+            {
+            }
+        }
+
+        public class MyWorkPage
+        {
+            public MyWorkPage(MyConfig config, MyWorkObject currentItem)
+            {
+            }
         }
     }
 }
